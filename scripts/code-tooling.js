2e51863529172c617a471d69675b5bb83025dfba
Chris Stead <cmstead@gmail.com>
2020-08-13 23:58:28 -0700

    Added jump-to dropdown for easier code navigation

--- scripts/code-tooling.js
+++ scripts/code-tooling.js
@@ -5,6 +5,7 @@
     const newProgramButton = document.getElementById('new-program');
     const codeExampleSelect = document.getElementById('load-example');
     const fullScreenLink = document.getElementById('view-full-screen');
+    const functionDeclarationsSelect = document.getElementById('function-declarations');
 
     const CURRENT_PROGRAM = 'currentProgram';
 
@@ -31,6 +32,7 @@
         const selectedExampleName = exampleCodeSelectElement.options[selectedIndex].value;
 
         loadSource(selectedExampleName);
+        setFunctionDeclarations();
     });
 
     newProgramButton.addEventListener('click', function (event) {
@@ -49,7 +51,7 @@
 
         setTimeout(function () {
             runButton.disabled = true;
-            
+
             runProgram()
                 .then(function () {
                     runButton.disabled = false;
@@ -108,33 +110,114 @@
             exampleCodeSelectElement.appendChild(option);
         });
 
-    const searchMap = decodeURI(window.location.href)
-        .split('?')
-        .pop()
-        .split('&')
-        .map(token => token.split('='))
-        .reduce((finalMap, [key, value]) => {
-            finalMap[key] = value;
-            return finalMap;
-        }, {});
-
-    if (typeof searchMap.source === 'string') {
-        const intervalId = setInterval(function () {
-            if (typeof editor !== 'undefined') {
-                clearInterval(intervalId);
-
-                loadSource(searchMap.source);
-                editor.setOption("readOnly", true);
-
-                const option = Array.prototype.slice.call(exampleCodeSelectElement.options, 0)
-                    .find(option => option.value === searchMap.source);
-
-                option.selected = true;
-
-                codeExampleSelect.disabled = true;
-                exampleCodeSelectElement.disabled = true;
-                newProgramButton.disabled = true;
-            }
-        }, 50);
+    function getSearchMap() {
+        return decodeURI(window.location.href)
+            .split('?')
+            .pop()
+            .split('&')
+            .map(token => token.split('='))
+            .reduce((finalMap, [key, value]) => {
+                finalMap[key] = value;
+                return finalMap;
+            }, {});
     }
+
+    function setSourceFromQuery() {
+        const searchMap = getSearchMap();
+
+        if (typeof searchMap.source === 'string') {
+            loadSource(searchMap.source);
+            editor.setOption("readOnly", true);
+
+            const option = Array.prototype
+                .slice.call(exampleCodeSelectElement.options, 0)
+                .find(option => option.value === searchMap.source);
+
+            option.selected = true;
+
+            codeExampleSelect.disabled = true;
+            exampleCodeSelectElement.disabled = true;
+            newProgramButton.disabled = true;
+        }
+    }
+
+    let lastTimeoutId = -1;
+
+    function getSecondsInMilliseconds(seconds) {
+        return seconds * 1000;
+    }
+
+    function setFunctionDeclarations() {
+        const editorSource = editor.getValue();
+        const parsedSource = parseSource(editorSource);
+
+        functionDeclarationsSelect.innerHTML = "<option value=\"\">Jump To</option>";
+        
+        const functionsOptGroup = document.createElement('optgroup');
+        
+        functionsOptGroup.setAttribute('label', 'Function Declarations');
+        functionDeclarationsSelect.appendChild(functionsOptGroup);
+        
+        const constantsOptGroup = document.createElement('optgroup');
+
+        constantsOptGroup.setAttribute('label', 'Constant Defintions');
+        functionDeclarationsSelect.appendChild(constantsOptGroup);
+
+        parsedSource.body.forEach(function (node) {
+
+            if (node.type === 'FunctionDeclaration') {
+                const line = node.line;
+                const name = node.name.originalName;
+
+                const newOption = document.createElement('option');
+                newOption.value = line;
+                newOption.textContent = name;
+
+                functionsOptGroup.appendChild(newOption);
+            } else if (node.type === 'InitializationExpression' && node.variableType === 'define') {
+                const line = node.line;
+                const name = node.identifier.originalName;
+
+                const newOption = document.createElement('option');
+                newOption.value = line;
+                newOption.textContent = name;
+
+                constantsOptGroup.appendChild(newOption);
+            }
+        });
+    }
+
+    functionDeclarationsSelect.addEventListener('change', function (event) {
+        const selectedIndex = functionDeclarationsSelect.selectedIndex;
+        const option = functionDeclarationsSelect[selectedIndex];
+        const position = parseInt(option.value);
+
+        if (!isNaN(position)) {
+            editor.focus();
+            editor.setCursor(position - 1, 0)
+        }
+    });
+
+    const intervalId = setInterval(function () {
+        if (typeof editor !== 'undefined') {
+            clearInterval(intervalId);
+
+            setSourceFromQuery();
+            setFunctionDeclarations();
+
+            editor.on('change', function () {
+                lastChange = Date.now();
+
+
+                saveCurrentSource(editor);
+
+                clearTimeout(lastTimeoutId);
+
+                lastTimeoutId = setTimeout(function () {
+                    setFunctionDeclarations()
+                }, getSecondsInMilliseconds(5));
+            });
+        }
+    }, 50);
+
 })();
